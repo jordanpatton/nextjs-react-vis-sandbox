7f78961339cb2346b9bf875a1bc4e0a41e56fc7d
Jordan Patton <jordanpatton@gmail.com>
2020-07-14 19:58:48 -0700

    install sass

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { AppProps as NextAppProps } from 'next/app'
 
+import 'react-vis/dist/main.scss';
+
 const App = ({ Component, pageProps }: NextAppProps) => <Component {...pageProps} />;
 
 export default App;
