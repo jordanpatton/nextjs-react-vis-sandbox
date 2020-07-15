20bc77ce987c6a068132d15bee4ef3ac83d23162
Jordan Patton <jordanpatton@gmail.com>
2020-07-14 20:32:59 -0700

    show lead-in and lead-out

--- components/GanttChart.tsx
+++ components/GanttChart.tsx
@@ -3,36 +3,53 @@ import { HorizontalBarSeries, VerticalGridLines, XAxis, XYPlot } from 'react-vis
 
 import { IEvent, IObservation } from '../common/types';
 
-const COLORS = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const DARK_COLORS = ['DarkRed', 'DarkOrange', 'DarkGreen', 'DarkBlue', 'DarkViolet'];
+const LIGHT_COLORS = ['Red', 'Orange', 'Green', 'Blue', 'Violet'];
 
 export class GanttChart extends React.Component<{ events: IEvent[] }> {
     /**
      * Splits data into transparent offset data and opaque actual data. Bar charts need
-     * two stacked data series in order to display a bar that doesn't start at the axis.
+     * stacked data series in order to display bars that don't start at the axis.
      */
     private getHorizontalBarSeriesData() {
         const { events } = this.props;
-        const actualData: IObservation[] = [];
+        const eventData: IObservation[] = [];
+        const leadInData: IObservation[] = [];
+        const leadOutData: IObservation[] = [];
         const offsetData: IObservation[] = [];
 
         // note: do not sort here; use order from provided data
         events.forEach((val, idx) => {
-            const absoluteStart = val.eventStartTimeMs - val.leadInDurationMs;
-            const absoluteEnd = val.eventStartTimeMs + val.eventDurationMs + val.leadOutDurationMs;
-            const offsetX = absoluteStart;
+            // offset
+            const offsetX = val.eventStartTimeMs - val.leadInDurationMs;
             offsetData[idx] = {
                 x: offsetX < 0 ? 0 : offsetX, // no negative offset
                 y: idx, // 0...N (not for display)
             };
-            const actualX = absoluteEnd - absoluteStart;
-            actualData[idx] = {
-                x: actualX < 1 ? 1 : actualX, // minimum bar width of 1
+            // lead-in
+            const leadInX = val.leadInDurationMs;
+            leadInData[idx] = {
+                x: leadInX < 1 ? 1 : leadInX, // minimum bar width of 1
                 y: idx, // 0...N (matches offset)
-                color: COLORS[idx % COLORS.length],
+                color: LIGHT_COLORS[idx % LIGHT_COLORS.length],
+            };
+            // event
+            const eventX = val.eventDurationMs;
+            eventData[idx] = {
+                x: eventX < 1 ? 1 : eventX, // minimum bar width of 1
+                y: idx, // 0...N (matches offset)
+                color: DARK_COLORS[idx % DARK_COLORS.length],
+            };
+            // lead-out
+            const leadOutX = val.leadOutDurationMs;
+            leadOutData[idx] = {
+                x: leadOutX < 1 ? 1 : leadOutX, // minimum bar width of 1
+                y: idx, // 0...N (matches offset)
+                color: LIGHT_COLORS[idx % LIGHT_COLORS.length],
             };
         });
 
-        return { actualData, offsetData };
+        return { eventData, leadInData, leadOutData, offsetData };
     }
 
     private getXDomainLower(): number {
@@ -59,7 +76,7 @@ export class GanttChart extends React.Component<{ events: IEvent[] }> {
 
     render() {
         const { events } = this.props;
-        const { actualData, offsetData } = this.getHorizontalBarSeriesData();
+        const { eventData, leadInData, leadOutData, offsetData } = this.getHorizontalBarSeriesData();
         return (
             <XYPlot
                 height={500}
@@ -85,7 +102,19 @@ export class GanttChart extends React.Component<{ events: IEvent[] }> {
                 {/* @ts-ignore: use `data` instead of `barWidth` */}
                 <HorizontalBarSeries
                     colorType="literal" // uses "color" from data itself
-                    data={actualData}
+                    data={leadInData}
+                    stack
+                />
+                {/* @ts-ignore: use `data` instead of `barWidth` */}
+                <HorizontalBarSeries
+                    colorType="literal" // uses "color" from data itself
+                    data={eventData}
+                    stack
+                />
+                {/* @ts-ignore: use `data` instead of `barWidth` */}
+                <HorizontalBarSeries
+                    colorType="literal" // uses "color" from data itself
+                    data={leadOutData}
                     stack
                 />
             </XYPlot>
